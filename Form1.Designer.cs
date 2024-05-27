20e27e5041c5b8584fef3398a5cae14b1d6f7859
jodahyeok <jaden0524@naver.com>
2024-05-27 17:02:14 +0900

    폼1 완성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -36,28 +43,102 @@
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Controls.Add(button3, 0, 1);
+            tableLayoutPanel1.Controls.Add(button4, 1, 1);
             tableLayoutPanel1.Dock = DockStyle.Bottom;
-            tableLayoutPanel1.Location = new Point(0, 411);
+            tableLayoutPanel1.Location = new Point(0, 90);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(534, 100);
+            tableLayoutPanel1.Size = new Size(534, 421);
             tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = SystemColors.HighlightText;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(261, 204);
+            button1.TabIndex = 0;
+            button1.Text = "한식";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.ForeColor = SystemColors.HighlightText;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(270, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(261, 204);
+            button2.TabIndex = 1;
+            button2.Text = "일식";
+            button2.TextAlign = ContentAlignment.BottomCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.ForeColor = SystemColors.HighlightText;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(3, 213);
+            button3.Name = "button3";
+            button3.Size = new Size(261, 205);
+            button3.TabIndex = 2;
+            button3.Text = "중식";
+            button3.TextAlign = ContentAlignment.BottomCenter;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button4.ForeColor = SystemColors.HighlightText;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(270, 213);
+            button4.Name = "button4";
+            button4.Size = new Size(261, 205);
+            button4.TabIndex = 3;
+            button4.Text = "양식\r\n";
+            button4.TextAlign = ContentAlignment.BottomCenter;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(89, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(359, 47);
+            label1.TabIndex = 1;
+            label1.Text = "점심 메뉴를 고르시오";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(534, 511);
+            Controls.Add(label1);
             Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
     }
 }
