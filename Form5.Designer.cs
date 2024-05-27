e7d5218e76fc60662b2f93e87b18728691dcf635
jodahyeok <jaden0524@naver.com>
2024-05-27 16:30:06 +0900

    양식 글씨 추가

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -38,26 +39,40 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.Dock = DockStyle.Bottom;
-            tableLayoutPanel1.Location = new Point(0, 411);
+            tableLayoutPanel1.Location = new Point(0, 90);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(534, 100);
+            tableLayoutPanel1.Size = new Size(534, 421);
             tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(87, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(359, 47);
+            label1.TabIndex = 1;
+            label1.Text = "양식 종류를 구하시오";
+            label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // Form5
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(534, 511);
+            Controls.Add(label1);
             Controls.Add(tableLayoutPanel1);
             Name = "Form5";
             Text = "Form5";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
     }
 }
