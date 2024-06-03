ee6ca270239ea83749d3091b0c2df055ad1db5c6
jodahyeok <jaden0524@naver.com>
2024-06-03 14:09:45 +0900

    중식 면 추천

---
+++ Form10.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MJC_Foodproject
+{
+    partial class Form10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button2 = new Button();
+            button3 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(button2, 0, 1);
+            tableLayoutPanel1.Controls.Add(button3, 0, 2);
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button5, 0, 3);
+            tableLayoutPanel1.Controls.Add(button6, 0, 4);
+            tableLayoutPanel1.Location = new Point(0, 56);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.Size = new Size(534, 452);
+            tableLayoutPanel1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.Location = new Point(3, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(528, 84);
+            button2.TabIndex = 1;
+            button2.Text = "짬뽕";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.Location = new Point(3, 183);
+            button3.Name = "button3";
+            button3.Size = new Size(528, 84);
+            button3.TabIndex = 2;
+            button3.Text = "간짜짱";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button5.Location = new Point(3, 273);
+            button5.Name = "button5";
+            button5.Size = new Size(528, 84);
+            button5.TabIndex = 4;
+            button5.Text = "쟁반짜장";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button6.Location = new Point(3, 363);
+            button6.Name = "button6";
+            button6.Size = new Size(528, 86);
+            button6.TabIndex = 5;
+            button6.Text = "차돌짬뽕";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(528, 84);
+            button1.TabIndex = 0;
+            button1.Text = "짜장면";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(82, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(370, 47);
+            label1.TabIndex = 8;
+            label1.Text = "이 음식을 추천합니다!";
+            // 
+            // Form10
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(534, 511);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label1);
+            MaximumSize = new Size(550, 550);
+            MinimumSize = new Size(550, 550);
+            Name = "Form10";
+            Text = "Form10";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button2;
+        private Button button3;
+        private Button button5;
+        private Button button6;
+        private Button button1;
+        private Label label1;
+    }
+}
