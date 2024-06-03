bd835f9c6dd1da68aa8beed678fd2643e6116357
kyh <USER@DESKTOP-2FI8QEE>
2024-06-03 23:10:37 +0900

    ㄴㄷㅊ

--- Form14.cs
+++ Form14.cs
@@ -21,16 +21,16 @@ namespace MJC_Foodproject
 
         private void Form14_Load(object sender, EventArgs e)
         {
-            string[] kr = { "국밥", "제육", "순두부찌개", "부재찌개", "불고기", "뼈해장국", "돌솥 비빔밥",
+            string[] kr = { "국밥", "제육볶음", "순두부찌개", "부대찌개", "불고기", "뼈해장국", "돌솥 비빔밥",
                                 "콩국수","라면", "비빔국수", "잔치국", "냉면", "칼국수", "막국수" };
             string[] jp = { "돈가스", "장어덮밥", "초밥", "카레라이스", "규동", "가츠동", "연어덮밥", "우동", "라멘", "메밀소바", "야키소바",
                             "마제소바" };
 
-            string[] ch = { "계란볶음밥", "짜장밥", "마파두부", "짬봉밥", "잡탕밥", "차오판", "잡채밥" , "짜장면", "짬뽕", "간짜장", "쟁반짜장",
+            string[] ch = { "계란볶음밥", "짜장밥", "마파두부", "짬뽕밥", "잡탕밥", "차오판", "잡채밥" , "짜장면", "짬뽕", "간짜장", "쟁반짜장",
                             "차돌짬뽕" };
 
             string[] am = { "샌드위치", "햄버거", "피자", "토스트", "빵" , "스테이크", "버팔로윙", "치킨", "케밥", "라자냐", "햄버거",
-                            "토마토", "스파게티", "크림", "스파게티", "로제" , "파스타", "알리오 올리오", "먹물 파스타", "해물 스파게티"};
+                            "토마토 스파게티", "크림 스파게티", "로제 파스타", "알리오 올리오", "먹물 파스타", "해물 스파게티"};
 
             Random random = new Random();
             int a = random.Next(0, 4); //0<=x<4
@@ -38,7 +38,7 @@ namespace MJC_Foodproject
             if (a == 0)
             {
                 Random randoms = new Random();
-                int b = randoms.Next(0, 15);
+                int b = randoms.Next(0, 14);
                 label1.Text = kr[b];
 
             }
@@ -46,21 +46,21 @@ namespace MJC_Foodproject
             else if (a == 1)
             {
                 Random randoms = new Random();
-                int b = randoms.Next(0, 13);
+                int b = randoms.Next(0, 12);
                 label1.Text = jp[b];
             }
 
             else if (a == 2)
             {
                 Random randoms = new Random();
-                int b = randoms.Next(0, 13);
+                int b = randoms.Next(0, 12);
                 label1.Text = ch[b];
             }
 
             else
             {
                 Random randoms = new Random();
-                int b = randoms.Next(0, 21);
+                int b = randoms.Next(0, 17);
                 label1.Text = am[b];
             }
         }
